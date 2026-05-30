74378094793f39529738a41801bcf92d270c3f9f
Raju Gorai <gorairaju699@gmail.com>
2026-05-30 11:36:28 +0530

    Update HelloWorld.java

--- Basics/HelloWorld.java
+++ Basics/HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+class firstclass{
+    public static void main(String args[]){
+        System.out.println("hello world");
     }
 }
