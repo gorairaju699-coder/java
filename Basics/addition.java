169eb3698138ba8b5ffa5386ba9d4f713f537732
Raju Gorai <gorairaju699@gmail.com>
2026-06-02 12:03:01 +0530

    Update addition.java

--- Basics/addition.java
+++ Basics/addition.java
@@ -1,1 +1,15 @@
+import java.util.*;
+public class addition {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter number:");
+        int a= sc.nextInt();
+       System.out.print("enter number:");
+       int b= sc.nextInt();
+       int sum= a+b;
+       System.out.print(sum);
+
+
+    }
+}
 
