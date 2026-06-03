1e79f0299d2714c14ac2ac2ba595236d08ad3996
Raju Gorai <gorairaju699@gmail.com>
2026-06-03 10:27:40 +0530

    Update twonumber_larger.java

--- Basics/twonumber_larger.java
+++ Basics/twonumber_larger.java
@@ -4,7 +4,10 @@ public class twonumberlarger {
         Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
         int b= sc.nextInt();
-        if (a>b){
+        if(a==b){
+            System.out.println("both are equal");
+        }
+        else  if (a>b){
             System.out.println("a is larger");
         }
         else{
