fa84ed9c34cde1f22462136a30ea3788597976d1
Raju Gorai <gorairaju699@gmail.com>
2026-06-03 10:23:27 +0530

    Update twonumber_larger.java

--- Basics/twonumber_larger.java
+++ Basics/twonumber_larger.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+public class twonumberlarger {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        if (a>b){
+            System.out.println("a is larger");
+        }
+        else{
+            System.out.println("b is larger");
+        }
+    }
+}
