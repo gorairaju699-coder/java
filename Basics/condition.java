40b116b2c644c61811aabf47417367fa13b07c12
Raju Gorai <gorairaju699@gmail.com>
2026-06-03 10:07:48 +0530

    Update condition.java

--- Basics/condition.java
+++ Basics/condition.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+public class ifcondition {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int age=sc.nextInt();
+        if (age>=18){
+            System.out.println("adult");
+        }
+        else{
+            System.out.println("not adult");
+        }
+    }
+    
+}
 
