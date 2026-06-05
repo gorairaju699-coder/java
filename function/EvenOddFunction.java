22d96f84ef6714bf9ea47791f5cd61c24589821f
Raju Gorai <gorairaju699@gmail.com>
2026-06-05 12:39:12 +0530

    Update EvenOddFunction.java

--- function/EvenOddFunction.java
+++ function/EvenOddFunction.java
@@ -1,1 +1,27 @@
+package function;
 
+import java.util.Scanner;
+
+public class oddeven {
+
+    public static boolean evenodd(int a) {
+        if (a % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        boolean result = evenodd(a);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
