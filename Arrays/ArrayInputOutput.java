50e0996b7799fe91c5ce287a2accc91e3b73a8dd
Raju Gorai <gorairaju699@gmail.com>
2026-06-06 12:10:05 +0530

    Update ArrayInputOutput.java

--- Arrays/ArrayInputOutput.java
+++ Arrays/ArrayInputOutput.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Array {
+public class ArrayInputOutput {
     public static void main(String args[]){
         Scanner sc =new Scanner(System.in);
         int size=sc.nextInt();
